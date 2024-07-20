b0dea65a4e63f0c1a61f65b3f871c9bc5f74b9f4
Jitty Thomas <jittythomas774@gmail.com>
2024-07-21 01:32:27 +0530

    signup

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -7,3 +7,27 @@ module.exports.login = async (req, res, next) => {
     next(ex);
   }
 };
+
+module.exports.register = async (req, res, next) => {
+  try {
+    const { username, email, password, publicKey } = req.body;
+    const usernameCheck = await User.findOne({ username });
+    if (usernameCheck)
+      return res.json({ msg: "Username already used", status: false });
+    const emailCheck = await User.findOne({ email });
+    if (emailCheck)
+      return res.json({ msg: "Email already used", status: false });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      email,
+      username,
+      password: hashedPassword,
+      publicKey,
+    });
+
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (ex) {
+    next(ex);
+  }
+};
