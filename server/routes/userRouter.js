c26763c0947941a46039ba3fc7e70abb4aaf21dd
Jitty Thomas <jittythomas774@gmail.com>
2024-07-21 15:45:18 +0530

    get all users

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,3 +1,7 @@
+const { getAllUsers } = require("../controllers/UserController");
+
 const router = require("express").Router();
+
+router.get("/allusers/:id", getAllUsers);
 
 module.exports = router;
