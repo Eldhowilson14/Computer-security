9dd42180bf00ad91fc94ae2af6fd13e63541b0a7
Tibi Sunny <tibisunny42@gmail.com>
2024-07-21 16:19:49 +0530

    minor change

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,12 +1,14 @@
 const {
   login,
   googleAuth,
-  logOut
+  logOut,
+  register
 } = require("../controllers/AuthController");
 
 const router = require("express").Router();
 
 router.post("/login", login);
+router.post("/register", register);
 router.post("/google-auth", googleAuth);
 router.get("/logout/:id", logOut);
 
