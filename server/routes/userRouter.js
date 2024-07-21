9e9f3bd0227bc07491abc94ec4ff5a2bdbb456fa
Tibi Sunny <tibisunny42@gmail.com>
2024-07-21 15:47:33 +0530

    set avatar

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,5 +3,6 @@ const { getAllUsers } = require("../controllers/UserController");
 const router = require("express").Router();
 
 router.get("/allusers/:id", getAllUsers);
+router.post("/setavatar/:id", setAvatar);
 
 module.exports = router;
